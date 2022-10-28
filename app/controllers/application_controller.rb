1a5403aa658b0f1f65f38b22098720dde05bb599
ritsuki-cs <game.ricky.x@gmail.com>
2022-10-28 18:41:24 +0000

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
   
-  def hello
-    render html: "hello, world!"
-  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,11 @@ Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   fixtures :all
+  
+  # テストユーザーがログイン中の場合にtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+  
   include ApplicationHelper
 end
