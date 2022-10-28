bee82ebbdfe1a69e083eb469cad4d82b06ef2359
ritsuki-cs <game.ricky.x@gmail.com>
2022-10-28 20:31:36 +0000

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require_relative '../config/environment'
 require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
+include ApplicationHelper
 
 class ActiveSupport::TestCase
   fixtures :all
@@ -12,5 +13,19 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
   
-  include ApplicationHelper
+  # テストユーザーとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+  
+end
+
+class ActionDispatch::IntegrationTest
+  
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
